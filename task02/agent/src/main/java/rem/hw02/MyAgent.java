b7e25d17f571203b66ea32b1b964c6d42897ded4
Reomor <reomor@gmail.com>
2019-02-03 20:47:02 +0300

    task02 fix agent, add abstract factory

--- task02/agent/src/main/java/rem/hw02/MyAgent.java
+++ task02/agent/src/main/java/rem/hw02/MyAgent.java
@@ -6,28 +6,26 @@ import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
 
 public class MyAgent implements ClassFileTransformer {
-	
-	static Instrumentation instrumentation = null;
-	
+
+    private static Instrumentation instrumentation = null;
+
     public static void premain(String agentArgs, Instrumentation inst) {
         initialize(agentArgs, inst, true);
     }
-    
+
     public static void agentmain(String agentArgs, Instrumentation inst) {
         initialize(agentArgs, inst, false);
     }
-	
-	public static void initialize(String agentArgs, Instrumentation inst, boolean isPremain) {
-	    MyAgent.instrumentation = inst;
-	    inst.addTransformer(new MyAgent(), true);
-	}
+
+    public static void initialize(String agentArgs, Instrumentation inst, boolean isPremain) {
+        MyAgent.instrumentation = inst;
+        inst.addTransformer(new MyAgent(), true);
+    }
 
     public byte[] transform(ClassLoader loader, String className,
-            Class<?> classBeingRedefined, ProtectionDomain protectionDomain,
-            byte[] classfileBuffer) throws IllegalClassFormatException {
-
-        System.out.println("Basic Agent: " + className + " : " + loader);
-        
+                            Class<?> classBeingRedefined, ProtectionDomain protectionDomain,
+                            byte[] classfileBuffer) throws IllegalClassFormatException {
+        //System.out.println("Basic Agent: " + className + " : " + loader);
         return null;
     }
 
