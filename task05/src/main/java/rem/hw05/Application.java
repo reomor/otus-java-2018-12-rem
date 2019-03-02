bd47e7a0545f2e4f4947922cd4cf0d66b2b9d346
Reomor <reomor@gmail.com>
2019-03-02 16:32:59 +0300

    task05 add base reflection helper

--- task05/src/main/java/rem/hw05/Application.java
+++ task05/src/main/java/rem/hw05/Application.java
@@ -1,19 +1,7 @@
 package rem.hw05;
 
-import java.util.Arrays;
-import java.util.function.IntFunction;
-
 public class Application {
-
     public static void main(String[] args) {
-        Object[] argz = new Object[]{1, "abc", 3.0, 'a'};
-        Class<?>[] classes = Arrays.stream(argz).map(s -> s.getClass()).toArray(value -> new Class<?>[value]);
-        //Class<?>[] classezz = Arrays.stream(argz).map(s -> s.getClass()).toArray(value -> new Class[value]);
-        System.out.println();
-        final Class<?>[] classez = new Class<?>[argz.length];
-        for (int i = 0; i < argz.length; i++) {
-            classez[i] = argz[i].getClass();
-        }
-        System.out.println();
+
     }
 }
