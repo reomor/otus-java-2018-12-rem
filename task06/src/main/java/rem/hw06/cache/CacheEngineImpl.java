dfd42af1094ea6bcbd3b18f48660b9deecf62a08
Reomor <reomor@gmail.com>
2019-03-03 14:25:54 +0300

    task06 cache engine put and get

--- task06/src/main/java/rem/hw06/cache/CacheEngineImpl.java
+++ task06/src/main/java/rem/hw06/cache/CacheEngineImpl.java
@@ -1,19 +1,16 @@
 package rem.hw06.cache;
 
 import java.lang.ref.SoftReference;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Timer;
+import java.util.*;
+import java.util.function.Function;
 
 public class CacheEngineImpl<K, V> implements CacheEngine<K, V> {
-    private static final int TIME_THRESHOLD_MS = 5; //порог отличия
-
     private final int maxElements;
     private final long lifeTimeMs;
     private final long idleTimeMs;
     private final boolean isEternal;
 
-    private final Map<K, SoftReference<CacheElement<K, V>>> elements = new HashMap<>();
+    private final Map<K, SoftReference<CacheElement<K, V>>> softReferenceMap = new HashMap<>();
     private final Timer timer = new Timer();
 
     public CacheEngineImpl(int maxElements, long lifeTimeMs, long idleTimeMs, boolean isEternal) {
@@ -25,11 +22,50 @@ public class CacheEngineImpl<K, V> implements CacheEngine<K, V> {
 
     @Override
     public void put(CacheElement<K, V> element) {
+        // effectively remove random element if max size is reached
+        if (softReferenceMap.size() == maxElements) {
+            softReferenceMap.keySet()
+                    .stream()
+                    .skip((int) (softReferenceMap.size() * Math.random()))
+                    .findFirst()
+                    .ifPresent(softReferenceMap::remove);
+        }
+        // map allows null as a key
+        K key = element.getKey();
+        softReferenceMap.put(key, new SoftReference<>(element));
 
+        if (!isEternal) {
+            if (lifeTimeMs != 0) {
+                TimerTask lifeTimerTask = getTimerTask(key, lifeElement -> lifeElement.getCreationTime() + lifeTimeMs);
+                timer.schedule(lifeTimerTask, lifeTimeMs);
+            }
+            if (idleTimeMs != 0) {
+                TimerTask idleTimerTask = getTimerTask(key, idleElement -> idleElement.getLastAccessTime() + idleTimeMs);
+                timer.schedule(idleTimerTask, idleTimeMs, idleTimeMs);
+            }
+        }
     }
 
     @Override
     public CacheElement<K, V> get(K key) {
-        return null;
+        SoftReference<CacheElement<K, V>> cacheElementSoftReference = softReferenceMap.get(key);
+        CacheElement<K, V> element = null;
+        if((element = cacheElementSoftReference.get()) != null) {
+            element.refreshAccessTime();
+        }
+        return cacheElementSoftReference.get();
+    }
+
+    private TimerTask getTimerTask(final K key, Function<CacheElement<K, V>, Long> timeFunction) {
+        return new TimerTask() {
+            @Override
+            public void run() {
+                CacheElement<K, V> cacheElement = softReferenceMap.get(key).get();
+                if (cacheElement == null || (timeFunction.apply(cacheElement) < System.currentTimeMillis())) {
+                    softReferenceMap.remove(key);
+                    this.cancel();
+                }
+            }
+        };
     }
 }
