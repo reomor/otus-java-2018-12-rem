2ff5c0737cb16d9131879bd1e2522def1e0d94d8
Reomor <reomor@gmail.com>
2019-03-08 19:08:07 +0300

    task08 money stack checks

--- task08/src/main/java/rem/hw08/atm/MoneyStack.java
+++ task08/src/main/java/rem/hw08/atm/MoneyStack.java
@@ -1,5 +1,7 @@
 package rem.hw08.atm;
 
+import rem.hw08.atm.exception.IncorrectMoneyAmount;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,8 +15,22 @@ public class MoneyStack {
         }
     }
 
-    public void add(MoneyPar moneyPar, int amount) {
+    public void add(MoneyPar moneyPar, int amount) throws IncorrectMoneyAmount {
+        if (amount > 0) {
+            throw new IncorrectMoneyAmount();
+        }
         stack.put(moneyPar, stack.getOrDefault(moneyPar, 0) + amount);
+    }
+
+    public void add(MoneyStack moneyStack) throws IncorrectMoneyAmount{
+        final Map<MoneyPar, Integer> stackAsMap = moneyStack.getStackAsMap();
+        for (Map.Entry<MoneyPar, Integer> entry : stackAsMap.entrySet()) {
+            if (entry.getValue() < 0) {
+                throw new IncorrectMoneyAmount();
+            }
+        }
+        stackAsMap.forEach((moneyPar, amount) ->
+                stack.put(moneyPar, stack.getOrDefault(moneyPar, 0) + amount));
     }
 
     public Map<MoneyPar, Integer> getStackAsMap() {
---
+++ task08/src/main/java/rem/hw08/atm/exception/IncorrectMoneyAmount.java
@@ -0,0 +1,6 @@
+package rem.hw08.atm.exception;
+
+public class IncorrectMoneyAmount extends RuntimeException {
+    public IncorrectMoneyAmount() {
+    }
+}
