5df8abf93cab70b0e75acb15c98cd222df38a0c7
Reomor <reomor@gmail.com>
2019-03-23 22:06:16 +0300

    task09 serialize primitive, and primitive array

---
+++ task09/src/main/java/rem/hw09/JsonSerializator.java
@@ -0,0 +1,50 @@
+package rem.hw09;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import rem.hw09.reflection.ReflectionHelper;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public class JsonSerializator {
+    public String toJson(Object object) {
+        if (object == null) {
+            return "null";
+        }
+        final Class<?> objectClass = object.getClass();
+        if (object instanceof Number) {
+            return String.valueOf(object);
+        } else if (object instanceof String || object instanceof Character) {
+            return "\"" + String.valueOf(object) + "\"";
+        } else if (object instanceof Boolean) {
+            return String.valueOf(object);
+        } else if (objectClass.isArray()) {
+            return arrayToJson(object).toJSONString();
+        } else if (object instanceof Collection) {
+            System.out.println("is collection");
+        } else if (object instanceof Map) {
+            System.out.println("is map");
+        } else {
+            System.out.println("is class");
+        }
+        return "";
+    }
+
+    private JSONArray arrayToJson(Object object) {
+        JSONArray jsonArray = new JSONArray();
+        for (int i = 0; i < Array.getLength(object); i++) {
+            Object element = Array.get(object, i);
+            if (element instanceof Number || element instanceof Boolean || element instanceof String || element instanceof Character) {
+                jsonArray.add(element);
+            } else {
+                final String e = toJson(element);
+                jsonArray.add(e);
+            }
+        }
+        return jsonArray;
+    }
+}
---
+++ task09/src/test/java/rem/hw09/JsonZerializatorTest.java
@@ -0,0 +1,119 @@
+package rem.hw09;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JsonZerializatorTest {
+    private JsonSerializator serializator;
+
+    @BeforeEach
+    public void setUp() {
+        serializator = new JsonSerializator();
+    }
+
+    @Test
+    public void intTest() {
+        final int primitive = 1;
+        final String jsonStringExpected = "1";
+        final String jsonStringActual = serializator.toJson(primitive);
+        assertEquals(jsonStringExpected, jsonStringActual);
+    }
+
+    @Test
+    public void intBoxTest() {
+        final Integer primitive = 1;
+        final String jsonStringExpected = "1";
+        final String jsonStringActual = serializator.toJson(primitive);
+        assertEquals(jsonStringExpected, jsonStringActual);
+    }
+
+    @Test
+    public void byteTest() {
+        final byte primitive = 1;
+        final String jsonStringExpected = "1";
+        final String jsonStringActual = serializator.toJson(primitive);
+        assertEquals(jsonStringExpected, jsonStringActual);
+    }
+
+    @Test
+    public void byteBoxTest() {
+        final Byte primitive = 1;
+        final String jsonStringExpected = "1";
+        final String jsonStringActual = serializator.toJson(primitive);
+        assertEquals(jsonStringExpected, jsonStringActual);
+    }
+
+    @Test
+    public void boolTest() {
+        final boolean bool = true;
+        final String jsonStringExpected = "true";
+        final String jsonStringActual = serializator.toJson(bool);
+        assertEquals(jsonStringExpected, jsonStringActual);
+    }
+
+    @Test
+    public void boolBoxTest() {
+        final Boolean bool = true;
+        final String jsonStringExpected = "true";
+        final String jsonStringActual = serializator.toJson(bool);
+        assertEquals(jsonStringExpected, jsonStringActual);
+    }
+
+    @Test
+    public void stringTest() {
+        final String string = "test string";
+        final String jsonStringExpected = "\"" + string + "\"";
+        final String jsonStringActual = serializator.toJson(string);
+        assertEquals(jsonStringExpected, jsonStringActual);
+    }
+
+    @Test
+    public void charTest() {
+        final  char character = 'c';
+        final String jsonStringExpected = "\"" + character + "\"";
+        final String jsonStringActual = serializator.toJson(character);
+        assertEquals(jsonStringExpected, jsonStringActual);
+    }
+
+    @Test
+    public void charBoxTest() {
+        final  Character character = new Character('c');
+        final String jsonStringExpected = "\"" + character + "\"";
+        final String jsonStringActual = serializator.toJson(character);
+        assertEquals(jsonStringExpected, jsonStringActual);
+    }
+
+    @Test
+    public void arrayPrimitiveTest() {
+        final int[] array = {1, 2, 3, 4 , 5};
+        final String jsonStringExpected = "[1,2,3,4,5]";
+        final String jsonStringActual = serializator.toJson(array);
+        assertEquals(jsonStringExpected, jsonStringActual);
+    }
+
+    @Test
+    public void arrayStringTest() {
+        final String[] array = {"1", "2", "3"};
+        final String jsonStringExpected = "[\"1\",\"2\",\"3\"]";
+        final String jsonStringActual = serializator.toJson(array);
+        assertEquals(jsonStringExpected, jsonStringActual);
+    }
+
+    @Test
+    public void arrayClassTest() {
+        class A {
+            private int i = 1;
+
+            public A(int i) {
+                this.i = i;
+            }
+        }
+        final A[] array = {new A(1), new A(2)};
+        final String jsonStringExpected = "[]";
+        final String jsonStringActual = serializator.toJson(array);
+        assertEquals(jsonStringExpected, jsonStringActual);
+    }
+}
