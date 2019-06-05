bfe677f966772c937cd41a4c6df47a7a369208d6
Reomor <reomor@gmail.com>
2019-06-05 22:36:36 +0300

    task16 dbservice init

---
+++ task16/dbserver/src/main/java/rem/hw16/dbserver/dbcommon/ConnectionHelper.java
@@ -0,0 +1,20 @@
+package rem.hw16.dbserver.dbcommon;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+import java.util.StringJoiner;
+
+public class ConnectionHelper {
+    public static String getConnectionMetadata(Connection connection) throws SQLException {
+        final StringJoiner joiner = new StringJoiner("\n");
+        joiner.add("Autocommit: " + connection.getAutoCommit());
+        final DatabaseMetaData metaData = connection.getMetaData();
+        joiner.add("DB name: " + metaData.getDatabaseProductName());
+        joiner.add("DB version: " + metaData.getDatabaseProductVersion());
+        joiner.add("Driver name: " + metaData.getDriverName());
+        joiner.add("Driver version: " + metaData.getDriverVersion());
+        joiner.add("JDBC version: " + metaData.getJDBCMajorVersion() + '.' + metaData.getJDBCMinorVersion());
+        return joiner.toString();
+    }
+}
